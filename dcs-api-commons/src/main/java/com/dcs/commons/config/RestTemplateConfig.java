0949295a1fb162da8b50c94ecaf68b073957c421
qq <qqq@qq.com>
2021-02-06 20:37:43 +0800

    upddate

--- dcs-api-commons/src/main/java/com/dcs/commons/config/RestTemplateConfig.java
+++ dcs-api-commons/src/main/java/com/dcs/commons/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.dcs.commons.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
